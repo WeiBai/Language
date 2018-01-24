53105204fe6e9e725d4ac6dc36b7528fe085cef7
Wei Bai <baiweiyj@qq.com>
2018-01-24 08:33:26 +0000

    Added encoding format as utf-8.

--- singleline/views.py
+++ singleline/views.py
@@ -1,3 +1,10 @@
+# encoding=utf8
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+from django.utils.encoding import smart_str, smart_unicode
+
 import requests
 import json
 from django.shortcuts import render
@@ -13,6 +20,7 @@ def edit_page(request):
 
 def edit_action(request):
     title = request.POST.get('title')
+
     url = "http://api.meaningcloud.com/lang-2.0"
 
     payload = "key=1b2ea248af8252d4d3ef84d0ef6798ce&txt=" + title + "&url=&doc="
